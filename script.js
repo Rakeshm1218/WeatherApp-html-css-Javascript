49335a2b31812f499383ebf2914e9118cf4398bc
rakeshm888p@gmail.com <rakeshm888p@gmail.com>
2024-05-29 21:09:56 +0530

    added files

---
+++ script.js
@@ -0,0 +1,24 @@
+//WEATHER APP
+
+const weatherForm = document.querySelector('.weatherForm');
+const cityInput = document.querySelector('.cityInput');
+const card = document.querySelector('.card');
+const apiKey = "93eecdcf79dfc977df4a605f5fe6fe23";
+
+weatherForm.addEventListener('submit', (e) => {
+
+})
+
+
+async function getWeatherData(city){
+
+}
+
+function displayWeatherInfo(data){
+
+}
+
+
+function displayError(error){
+
+}
